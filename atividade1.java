70c8d939dd5287db5c0cb618d23bd90d52c86ece
Alfredo <a.hannzel@gmail.com>
2019-09-02 19:47:49 -0300

    atividade 1

--- atividade1.java
+++ atividade1.java
@@ -4,7 +4,7 @@ public class atividade1{
 		int cateto2=20;
 		double hipotenusa = teoremaPitagoras(cateto1,cateto2);
 	}
-	private double teoremaPitagoras(int x, int y){
+	private static double teoremaPitagoras(int x, int y){
 		return Math.sqrt((x*x) + (y*y));
 	}
 }
